7fc9c76fc9aa012b2c0b4b948d65dec2d0340ad4
rtrive <riccardo.trivellato@gmail.com>
2021-11-08 20:59:39 +0100

    chore: deploy

--- Build a Web3 App with Solidity + Ethereum Smart Contracts/my-wave-portal/scripts/run.js
+++ Build a Web3 App with Solidity + Ethereum Smart Contracts/my-wave-portal/scripts/run.js
@@ -1,20 +1,21 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
+    const [owner, randoPerson] = await hre.ethers.getSigners();
     const waveContractFactory = await hre.ethers.getContractFactory('WavePortal');
     const waveContract = await waveContractFactory.deploy();
     await waveContract.deployed();
-    console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
+
+    console.log('Contract deployed to:', waveContract.address);
+    console.log('Contract deployed by:', owner.address);
 
     let waveCount;
     waveCount = await waveContract.getTotalWaves();
 
-    const waveTxn = await waveContract.wave();
+    let waveTxn = await waveContract.wave();
     await waveTxn.wait();
 
     waveCount = await waveContract.getTotalWaves();
 
-    waveTxn = await waveContract.connect(randomPerson).wave();
+    waveTxn = await waveContract.connect(randoPerson).wave();
     await waveTxn.wait();
 
     waveCount = await waveContract.getTotalWaves();
